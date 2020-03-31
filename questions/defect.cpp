f3f467274e41ed166f3aaed41e26c04b28c159b4
Vibhav Aggarwal <vibhavaggarwal@Vibhavs-MacBook-Pro.local>
2020-04-01 04:14:29 +0530

    AC

--- questions/defect.cpp
+++ questions/defect.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int n, m;
 int g[50][50];
+bool connected[2501][2501];
+vector<vector<int> > graph;
 
 int c, curNode;
 vector<pair<int, int> > pts;
 
-void createGraph(int i, int j, vector<set<int> > &graph) {
+void createGraph(int i, int j) {
 	if (i < 0 || i >= n || j < 0 || j >= m) return;
 	int cur = g[i][j];
 	if (cur > 0) {
-		if (cur != curNode) {
-			graph[curNode].insert(cur);
-			graph[cur].insert(curNode);
+		if (cur != curNode && !connected[cur][curNode]) {
+			graph[curNode].push_back(cur);
+			graph[cur].push_back(curNode);
+			connected[cur][curNode] = true;
 		}
 		return;
 	}
@@ -24,28 +27,32 @@ void createGraph(int i, int j, vector<set<int> > &graph) {
 	}
 
 	g[i][j] = curNode;
-	createGraph(i+1, j, graph);
-	createGraph(i-1, j, graph);
-	createGraph(i, j+1, graph);
-	createGraph(i, j-1, graph);
+	createGraph(i+1, j);
+	createGraph(i-1, j);
+	createGraph(i, j+1);
+	createGraph(i, j-1);
 	return;
 }
 
-int BFSdepth(vector<set<int> > &graph, int src) {
-	bool visited[graph.size()];
-	for (int i = 1; i < graph.size(); i++) visited[i] = false;
-	set<int> sources; sources.insert(src);
-	set<int>::iterator it, it2;
+int BFSdepth(int src) {
+	int status[graph.size()];
+	fill(status, status+graph.size(), 0);		// 0 = inactive, 1 = active, 2 = visited
+
+	vector<int> sources; sources.push_back(src);
+	int it = 0, it2;
 	int ans = -1;
-	while (!sources.empty()) {
-		set<int> sources2;
-		for (it = sources.begin(); it != sources.end(); it++) {
-			visited[*it] = true;
-			for (it2 = graph[*it].begin(); it2 != graph[*it].end(); it2++) {
-				if (!visited[*it2]) sources2.insert(*it2);
+	while (it < sources.size()) {
+		int e = sources.size();
+		for (; it < e; it++) {
+			status[sources[it]] = 2;
+			for (it2 = 0; it2 < graph[sources[it]].size(); it2++) {
+				int ind = graph[sources[it]][it2];
+				if (status[ind] == 0) {
+					sources.push_back(ind);
+					status[ind] = 1;
+				}
 			}
 		}
-		sources = sources2;
 		ans++;
 	}
 	return ans;
@@ -62,25 +69,29 @@ int main() {
 			g[i][j] = temp-1;
 		}
 
-		vector<set<int> > graph;
-		graph.push_back(set<int>());
-		curNode = 0;
+		for (int i = 0; i < 2501; i++) for (int j = 0; j < 2501; j++) connected[i][j] = false;
+
+		graph.clear(); graph.push_back(vector<int>());
 		pts.clear(); pts.push_back(make_pair(0, 0));
+		curNode = 0;
 
 		while (!pts.empty()) {
 			vector<pair<int, int> > pts2 (pts);
+			pts.clear();
 			for (int i = 0; i < pts2.size(); i++) {
-				pts.clear();
-				graph.push_back(set<int>());
-				curNode++;
 				int p = pts2[i].first, q = pts2[i].second;
-				c = g[p][q];
-				createGraph(p, q, graph);
+
+				if (g[p][q] <= 0) {
+					graph.push_back(vector<int>());
+					curNode++;
+					c = g[p][q];
+					createGraph(p, q);
+				}
 			}
 		}
 
 		int ans = 2500;
-		for (int i = 1; i <= graph.size()-1; i++) ans = min(BFSdepth(graph, i), ans);
+		for (int i = 1; i < graph.size(); i++) ans = min(BFSdepth(i), ans);
 
 		cout << ans << endl;
 	}
