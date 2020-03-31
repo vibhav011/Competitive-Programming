06a0f0a440f461c6af7ba63323acfc20e5032b10
Vibhav Aggarwal <vibhavaggarwal@Vibhavs-MacBook-Pro.local>
2020-04-01 02:22:58 +0530

    AC

--- questions/codechef-march-lunchtime/mxmlcm.cpp
+++ questions/codechef-march-lunchtime/mxmlcm.cpp
@@ -1,29 +1,70 @@
 #include <bits/stdc++.h>
+#define M 10001
 using namespace std;
+vector<pair<int, int> > fact[M];
+vector<int> primes;
 
-long long int gcd(long long int a, long long int b) {			// a <= b
-	if (a > b) swap(a, b);
-	if (b%a == 0) return a;
-	return gcd(b%a, a);
-}
-
-long long int lcm(long long int a, long long int b) {
-	return a*b/gcd(a, b);
+int gcdRat (vector<pair<int, int> > &a, int lc[]) {
+	int ans = 1;
+	for (int j = 0; j < a.size(); j++) {
+		ans *= pow(a[j].first, a[j].second - min(a[j].second, lc[a[j].first]));
+	}
+	return ans;
 }
 
 int main() {
-	int t; cin >> t;
+	bool is_composite[M]; fill(is_composite, is_composite + M, false);
 
+	for (int i = 2; i < M; i++) {
+		if (!is_composite[i]) {
+			primes.push_back(i);
+			fact[i].push_back(make_pair(i, 1));
+		}
+		for (int j = 0; j < primes.size() && i*primes[j] < M; j++) {
+			is_composite[i*primes[j]] = true;
+			if (i % primes[j] == 0) {
+				fact[i*primes[j]] = fact[i];
+				break;
+			}
+			else {
+				fact[i*primes[j]] = fact[i];
+				fact[i*primes[j]].push_back(make_pair(primes[j], 1));
+			}
+		}
+	}
+
+	for (int i = 2; i < M; i++) {
+		if (fact[i][0].first != i) {
+			int i2 = i;
+			for (int j = 0; j < fact[i].size(); j++) {
+				int cnt = 0;
+				while (i2 % fact[i][j].first == 0) {
+					cnt++;
+					i2 /= fact[i][j].first;
+				}
+				fact[i][j].second = cnt;
+			}
+		}
+	}
+
+	int t; cin >> t;
 	while(t--) {
 		int n, m; cin >> n >> m;
-		long long int lc = 1;
+		int lc[m+1];
+		fill(lc, lc+m+1, 0);
 		for (int i = 0; i < n; i++) {
-			long long int temp; cin >> temp;
-			lc = lcm(lc, temp);
+			int temp; cin >> temp;
+			for (int j = 0; j < fact[temp].size(); j++) {
+				lc[fact[temp][j].first] = max(lc[fact[temp][j].first], fact[temp][j].second);
+			}
 		}
-		int j = 0;
-		for (int i = 1; i <= m; i++) {
-			if (gcd(i, lc) == 1) j = i;
+		int mx = 1, j = 1;
+		for (int i = 2; i <= m; i++) {
+			int tmx = gcdRat(fact[i], lc);
+			if (tmx > mx) {
+				mx = tmx;
+				j = i;
+			}
 		}
 		cout << j << endl;
 	}
